1f481bb5826c185dacbf0f5efd0bd6b773456ef3
WatcherMagic <oliviaw.randomnumber@gmail.com>
2025-07-06 14:55:56 -0500

    first commit

---
+++ dynamicpupspawns.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace RainWorld_Mod_Template
+{
+    //BEFORE LOADING MOD, ADD TO modinfo.json FOR EACH DEPENDENCY:
+
+    //hard dependencies:
+    //"requirements": ["mod_id_1", "mod_id_2", "etc"],
+    
+    //soft dependencies:
+    //"priorities": ["mod_id_1", "mod_id_2", "etc"]
+
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class DynamicPupSpawns : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "dynamicpupspawns";
+        public const string PLUGIN_NAME = "Dynamic Pup Spawns";
+        public const string PLUGIN_VERSION = "0.1";
+
+        private void OnEnable()
+        {
+            //
+        }
+
+    }
+}
