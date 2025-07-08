6444e2bf69150921f959f71da81829ea0fe80171
WatcherMagic <oliviaw.randomnumber@gmail.com>
2025-07-08 18:26:21 -0500

    - rewrote references in csproj - mod now functions correctly after switching to jetbrains rider - Added function to iterate over den nodes in region and return rooms with dens + number of dens in room

--- dynamicpupspawns.cs
+++ dynamicpupspawns.cs
@@ -7,7 +7,7 @@ namespace RainWorld_Mod_Template
 
     //hard dependencies:
     //"requirements": ["mod_id_1", "mod_id_2", "etc"],
-    
+
     //soft dependencies:
     //"priorities": ["mod_id_1", "mod_id_2", "etc"]
 
@@ -18,10 +18,57 @@ namespace RainWorld_Mod_Template
         public const string PLUGIN_NAME = "Dynamic Pup Spawns";
         public const string PLUGIN_VERSION = "0.1";
 
+        private Dictionary<AbstractRoom, int>? _validSpawnRooms = null;
         private void OnEnable()
         {
-            //
+            On.World.SpawnPupNPCs += SpawnPups;
         }
 
+        private int SpawnPups(On.World.orig_SpawnPupNPCs orig, World self)
+        {
+            Logger.LogInfo("SpawnPupOnWorldLoad() initiated");
+            Debug.Log("SpawnPupOnWorldLoad() initiated");
+
+            Logger.LogInfo("StorySession status: " + self.game.IsStorySession);
+            Debug.Log("StorySession status: " + self.game.IsStorySession);
+
+            //int pupsThisCycle = UnityEngine.Random.Range(0, 11);
+            //Debug.Log("Spawning " + pupsThisCycle + " pups");
+
+            _validSpawnRooms = GetRoomsWithDens(self);
+            string logMessege = "Rooms with Den Nodes:\n";
+            foreach (KeyValuePair<AbstractRoom, int> pair in _validSpawnRooms)
+            {
+                logMessege += pair.Key.name + " : " + pair.Value + "\n";
+            }
+            Logger.LogInfo(logMessege);
+            Debug.Log(logMessege);
+
+            return orig(self);
+        }
+        
+        private Dictionary<AbstractRoom, int> GetRoomsWithDens(World world)
+        {
+            Dictionary<AbstractRoom, int> roomsWithDens = new Dictionary<AbstractRoom, int>();
+            int densInRoom = 0;
+
+            foreach (AbstractRoom room in world.abstractRooms)
+            {
+                foreach (AbstractRoomNode node in room.nodes)
+                {
+                    if (node.type == AbstractRoomNode.Type.Den)
+                    {
+                        densInRoom++;
+                    }
+                }
+                if (densInRoom > 0)
+                {
+                    roomsWithDens.Add(room, densInRoom);
+                }
+                densInRoom = 0;
+            }
+
+            return roomsWithDens;
+        }
     }
 }
