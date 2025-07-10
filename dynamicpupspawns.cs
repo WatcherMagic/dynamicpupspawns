302346cd1b57c762126a4457ced3017c950e0f94
WatcherMagic <oliviaw.randomnumber@gmail.com>
2025-07-10 16:33:27 -0500

    Added functionality for weighted randomized room selection, currently commented out

--- dynamicpupspawns.cs
+++ dynamicpupspawns.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using BepInEx;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -17,7 +16,7 @@ namespace DynamicPupSpawns
     [BepInPlugin("dynamicpupspawns", "Dynamic Pup Spawns", "0.1")]
     public class DynamicPupSpawns : BaseUnityPlugin
     {
-        private readonly int _minPupsInRegion = 0;
+        private readonly int _minPupsInRegion = 5;
         private readonly int _maxPupsInRegion = 10;
         
         private void OnEnable()
@@ -29,18 +28,19 @@ namespace DynamicPupSpawns
         {
             //get rooms with unsubmerged den nodes
             Dictionary <AbstractRoom, int> validSpawnRooms = GetRoomsWithViableDens(self);
-            string logMessage = "Rooms with Den Nodes:\n";
-            foreach (KeyValuePair<AbstractRoom, int> pair in validSpawnRooms)
-            {
-                logMessage += pair.Key.name + " : " + pair.Value + "\n";
-            }
-            Logger.LogInfo(logMessage);
-            Debug.Log(logMessage);
+
+            // string logMessage = "Rooms with Den Nodes:\n";
+            // foreach (KeyValuePair<AbstractRoom, int> pair in validSpawnRooms)
+            // {
+            //     logMessage += pair.Key.name + " : " + pair.Value + "\n";
+            // }
+            // Logger.LogInfo(logMessage);
+            // Debug.Log(logMessage);
             
             //determine room spawn weight based on number of dens in room
             Dictionary<AbstractRoom, int> roomWeights = CalculateRoomSpawnWeight(validSpawnRooms);
             int totalSpawnWeight = 0;
-            logMessage = "Approximate chance for pup spawn per room:\n";
+            string logMessage = "Approximate chance for pup spawn per room:\n";
             foreach (KeyValuePair<AbstractRoom, int> pair in roomWeights)
             {
                 logMessage += pair.Key.name + " : " + pair.Value.ToString("00") + "%\n";
@@ -54,15 +54,20 @@ namespace DynamicPupSpawns
             Logger.LogInfo(pupNum + " pups this cycle");
             
             //sort dict of rooms and wights into parallel arrays in ascending order
-            AbstractRoom spawnRoom;
-            Dictionary<int[], AbstractRoom[]> sortedArrays = SortRooms(roomWeights);
-            Logger.LogInfo("Received sorted weights and rooms: " + sortedArrays);
+            //Dictionary<int[], AbstractRoom[]> sortedArrays = CreateParallelRoomWeightArrays(roomWeights);
+            CreateParallelRoomWeightArrays();
+            //Logger.LogInfo("Received sorted weights and rooms.");
+            //int[] weightsScale = AssignSortedRoomScaleValues(sortedArrays.ElementAt(0).Key);
+            AssignSortedRoomScaleValues();
+            //Logger.LogInfo("Received reassigned weights array.");
             
             //get random room for each pup
             for (int i = 0; i < pupNum; i++)
             {
-                spawnRoom = RandomPickRoomByWeight(sortedArrays.ElementAt(0).Key, sortedArrays.ElementAt(0).Value, totalSpawnWeight);
-                Logger.LogInfo(spawnRoom.name + " picked for Pup " + (i + 1));
+                RandomPickRoomByWeight();
+                // AbstractRoom spawnRoom = RandomPickRoomByWeight(weightsScale, sortedArrays.ElementAt(0).Value);
+                // Logger.LogInfo("Received randomly selected room.");
+                // Logger.LogInfo(spawnRoom.name + " picked for Pup " + (i + 1));
             }
 
             return orig(self);
@@ -70,6 +75,8 @@ namespace DynamicPupSpawns
 
         private Dictionary<AbstractRoom, int> GetRoomsWithViableDens(World world)
         {
+            Logger.LogInfo("Entered GetRoomsWithViableDens()");
+            
             Dictionary<AbstractRoom, int> roomsWithDens = new Dictionary<AbstractRoom, int>();
             
             int densInRoom = 0;
@@ -91,95 +98,125 @@ namespace DynamicPupSpawns
                     densInRoom = 0;
                 }
             }
+            Logger.LogInfo("Exited GetRoomWithViableDens()");
+            
             return roomsWithDens;
         }
 
         private Dictionary<AbstractRoom, int> CalculateRoomSpawnWeight(Dictionary<AbstractRoom, int> roomsAndDens)
         {
-            Dictionary<AbstractRoom, int> spawnWeights = new Dictionary<AbstractRoom, int>();
-        
-            int totalDens = 0;
-            foreach (KeyValuePair<AbstractRoom, int> pair in roomsAndDens)
-            {
-                totalDens += pair.Value;
-            }
+             Dictionary<AbstractRoom, int> spawnWeights = new Dictionary<AbstractRoom, int>();
             
-            int weight;
-            foreach (KeyValuePair<AbstractRoom, int> pair in roomsAndDens)
-            {
-                weight = Mathf.RoundToInt(pair.Value / (float)totalDens * 100);
-                spawnWeights.Add(pair.Key, weight);                    
-
-            }
+             int totalDens = 0;
+             foreach (KeyValuePair<AbstractRoom, int> pair in roomsAndDens)
+             {
+                 totalDens += pair.Value;
+             }
+            
+             int weight;
+             foreach (KeyValuePair<AbstractRoom, int> pair in roomsAndDens)
+             {
+                 weight = Mathf.RoundToInt(pair.Value / (float)totalDens * 100);
+                 spawnWeights.Add(pair.Key, weight);                    
+            
+             }
             return spawnWeights;
         }
 
-        private Dictionary<int[], AbstractRoom[]> SortRooms(Dictionary<AbstractRoom, int> roomWeights)
+        private /*Dictionary<int[], AbstractRoom[]>*/ void CreateParallelRoomWeightArrays(/*Dictionary<AbstractRoom, int> roomWeights*/)
         {
+            Logger.LogInfo("Entered CreateParallelRoomWeightArrays()");
+            
             //move weights and rooms into parallel arrays
-            int[] weights = new int[roomWeights.Count];
-            AbstractRoom[] rooms = new AbstractRoom[roomWeights.Count];
-            int index = 0;
-            foreach (KeyValuePair<AbstractRoom, int> pair in roomWeights)
-            {
-                weights[index] = pair.Value;
-                rooms[index] = pair.Key;
-                index++;
-            }
+            // int[] weights = new int[roomWeights.Count];
+            // AbstractRoom[] rooms = new AbstractRoom[roomWeights.Count];
+            // int index = 0;
+            // foreach (KeyValuePair<AbstractRoom, int> pair in roomWeights)
+            // {
+            //     weights[index] = pair.Value;
+            //     rooms[index] = pair.Key;
+            //     index++;
+            // }
             
             //sort parallel arrays weights[] and rooms[] by ascending weight (bubble sort)
-            bool swapped;
-            int tempWeight;
-            AbstractRoom tempRoom;
-            for (int i = 0; i < roomWeights.Count; i++)
-            {
-                swapped = false;
-                for (int x = 0; x < roomWeights.Count; x++)
-                {
-                    if (weights[x] > weights[x + 1])
-                    {
-                        tempWeight = weights[x];
-                        tempRoom = rooms[x];
-                        
-                        weights[x] = weights[x + 1];
-                        rooms[x] = rooms[x + 1];
-                        
-                        weights[x + 1] = tempWeight;
-                        rooms[x + 1] = tempRoom;
-                        
-                        swapped = true;
-                    }
-                }
-
-                if (!swapped)
-                {
-                    break;
-                }
-            }
-            Logger.LogInfo("Sorted arrays of weights and rooms:");
-            Logger.LogInfo(weights);
-            Logger.LogInfo(rooms);
-
-            return new Dictionary<int[], AbstractRoom[]> {{weights, rooms}};
+            // bool swapped;
+            // int tempWeight;
+            // AbstractRoom tempRoom;
+            // for (int i = 0; i < roomWeights.Count; i++)
+            // {
+            //     swapped = false;
+            //     for (int x = 0; x < roomWeights.Count; x++)
+            //     {
+            //         if (weights[x] > weights[x + 1])
+            //         {
+            //             tempWeight = weights[x];
+            //             tempRoom = rooms[x];
+            //             
+            //             weights[x] = weights[x + 1];
+            //             rooms[x] = rooms[x + 1];
+            //             
+            //             weights[x + 1] = tempWeight;
+            //             rooms[x + 1] = tempRoom;
+            //             
+            //             swapped = true;
+            //         }
+            //     }
+            //
+            //     if (!swapped)
+            //     {
+            //         break;
+            //     }
+            // }
+            // Logger.LogInfo("Sorted arrays of weights and rooms:");
+            // Logger.LogInfo("Assigned weights and rooms to parallel arrays.");
+            // Logger.LogInfo(weights);
+            // Logger.LogInfo(rooms);
+        
+            //return new Dictionary<int[], AbstractRoom[]> {{weights, rooms}};
         }
         
-        private AbstractRoom RandomPickRoomByWeight(int[] weightsArray, AbstractRoom[] roomsArray, int totalWeight)
+        private /*int[]*/ void AssignSortedRoomScaleValues(/*int[] weightsArray*/)
         {
-            Logger.LogInfo("Picking room...");
-
-            int roomIndex = 0;
-            int randNum = Random.Range(0, totalWeight + 1);
-            for (int i = 0; i < weightsArray.Length; i++)
-            {
-                if (i == weightsArray.Length || weightsArray[i] <= randNum && randNum <= weightsArray[i + 1])
-                {
-                    roomIndex = i;
-                    break;
-                }
-            }
+            Logger.LogInfo("Entered AssignSortedRoomScaleValues()");
             
-            Logger.LogInfo("Picked " + roomsArray[roomIndex].name);
-            return roomsArray[roomIndex];
+            // int scaleValue = 0;
+            // for (int i = 0; i < weightsArray.Length; i++)
+            // {
+            //     scaleValue += weightsArray[i];
+            //     weightsArray[i] = scaleValue;
+            // }
+            // Logger.LogInfo("Weights array was modified to correspond to total weight scale and will be returned!");
+            // Logger.LogInfo(weightsArray.ToString());
+            //
+            // return weightsArray;
+        }
+        
+        private /*AbstractRoom*/ void RandomPickRoomByWeight(/*int[] weightsArray, AbstractRoom[] roomsArray*/)
+        {
+            Logger.LogInfo("Entered RandomPickRoomByWeight()");
+            
+            // Logger.LogInfo("Picking room...");
+            // int totalWeight = weightsArray[weightsArray.Length - 1];
+            //
+            // int roomIndex = 0;
+            // int randNum = Random.Range(0, totalWeight + 1);
+            // Logger.LogInfo("Random number is " + randNum);
+            // for (int i = 0; i < weightsArray.Length; i++)
+            // {
+            //     if (i == weightsArray.Length)
+            //     {
+            //         Logger.LogInfo("Selected final index");
+            //         roomIndex = i;
+            //     }
+            //     else if (weightsArray[i] <= randNum && randNum <= weightsArray[i + 1])
+            //     {
+            //         Logger.LogInfo("Index selected is " + i + " (" + weightsArray[i] + " <= " + randNum + " <= " + weightsArray[i + 1] + ")");
+            //         roomIndex = i;
+            //         break;
+            //     }
+            // }
+            //
+            // return roomsArray[roomIndex];
         }
     }
 }
