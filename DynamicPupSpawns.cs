ca36d830599f685932388f09c39e9380afbc7fb3
WatcherMagic <oliviaw.randomnumber@gmail.com>
2025-07-23 15:12:03 -0500

    - Confirmed save data deletion does not interfere with dummy other data

--- DynamicPupSpawns.cs
+++ DynamicPupSpawns.cs
@@ -228,11 +228,14 @@ namespace dynamicpupspawns
         }
 
         private const string _SAVE_DATA_DELIMITER = "DynamicPupSpawnsData";
+        private const string _STR_SPLIT = "<WM.DPS>";
         private string SaveDataToString(On.MiscWorldSaveData.orig_ToString orig, MiscWorldSaveData self)
         {   
             String s = orig(self);
-
-            s = String.Concat(s, _SAVE_DATA_DELIMITER, "<mwA><someMod>some other mod's data<someMod>");
+            String data = _SAVE_DATA_DELIMITER + _STR_SPLIT + "PupID:Room" + _STR_SPLIT
+                + "PupID2:Room2" + _STR_SPLIT + "PupID3:Room3" + _STR_SPLIT;
+            
+            s = String.Concat(s, data, "<mwA><someMod>some other mod's data<someMod>");
             
             Logger.LogInfo("SaveDataToString string:\n" + s);
 
@@ -253,12 +256,19 @@ namespace dynamicpupspawns
                     modString = self.unrecognizedSaveStrings[i];
                     Logger.LogInfo("String found: " + modString);
                     self.unrecognizedSaveStrings.RemoveAt(i);
-                    //break;
+                    break;
                 }
+                
             }
             if (modString == null)
             {
                 Logger.LogInfo("Couldn't find mod save data!");
+            }
+            
+            Logger.LogInfo("Remaining unrecognizedSaveStrings:");
+            foreach (String u in self.unrecognizedSaveStrings)
+            {
+                Logger.LogInfo(u);
             }
             
             //do stuff with saved data
