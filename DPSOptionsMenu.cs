7a4cef20a2a4f6b7ed026d602afc7bcddb472eb0
WatcherMagic <oliviaw.randomnumber@gmail.com>
2025-09-17 18:12:22 -0700

    Added basic general settings (if pups spawn, if pups are persistence, spawn chance, and min/mix) to options menu

--- DPSOptionsMenu.cs
+++ DPSOptionsMenu.cs
@@ -1,15 +1,50 @@
-
+using Menu.Remix.MixedUI;
+using RWCustom;
+using UnityEngine;
 
 namespace dynamicpupspawns;
 
 public class DPSOptionsMenu : OptionInterface
 {
-    // public readonly Configurable<int> minPups;
-    // public readonly Configurable<int> maxPups;
+    public readonly Configurable<bool> pupsSpawn;
+    public readonly Configurable<bool> persistence;
+    public readonly Configurable<int> minPups;
+    public readonly Configurable<int> maxPups;
+    public readonly Configurable<int> spawnChance;
 
     public DPSOptionsMenu(DynamicPupSpawns plugin)
     {
-        // minPups = config.Bind<int>("WatcherMagic_DynamicPups_Min_Pups", 1);
-        // minPups = config.Bind<int>("WatcherMagic_DynamicPups_Max_Pups", 4);
+        
+        pupsSpawn = config.Bind<bool>("DPS_PupsSpawn", true);
+        persistence = config.Bind<bool>("DPS_Persistence", true);
+        minPups = config.Bind<int>("DPS_MinPups", 1);
+        maxPups = config.Bind<int>("DPS_MaxPups", 4);
+        spawnChance = config.Bind<int>("DPS_SpawnChance", 5);
+    }
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        OpTab generalSettings = new OpTab(this, Custom.rainWorld.inGameTranslator.Translate("General"));
+        Tabs = new []
+        {
+            generalSettings
+        };
+        
+        UIelement[] optionsUI =
+        {
+            new OpLabel(10f, 550f, "General Settings", true),
+            new OpCheckBox(pupsSpawn, 10f, 500f),
+            new OpLabel(50f, 500f, "Extra pup spawns"),
+            new OpCheckBox(persistence, 10f, 470f),
+            new OpLabel(50f, 470f, "Living pups respawn"),
+            new OpLabel(10f, 420f, "Extra pup spawns settings:"),
+            new OpSlider(spawnChance, new Vector2(10f, 380f), 100),
+            new OpDragger(minPups, 10f, 350f),
+            new OpLabel(50f, 350f, "Min Pups Spawned"),
+            new OpDragger(maxPups, 10f, 320f),
+            new OpLabel(50f, 320f, "Max Pups Spawned")
+        };
+        generalSettings.AddItems(optionsUI);
     }
 }
