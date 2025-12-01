87a27d97a9f19b127a968ab79458e4e895144758
WatcherMagic <oliviaw.randomnumber@gmail.com>
2025-12-01 16:16:23 -0700

    Added variable for "tamed" debug display

--- PersistentPupData.cs
+++ PersistentPupData.cs
@@ -2,14 +2,18 @@ namespace dynamicpupspawns;
 
 public class PersistentPupData
 {
-    public string ID { get; }
+    public string PupID { get; }
     public string Room { get; }
     public bool IsTame { get; }
+    public bool displayWasTamedInDebug { get; }
+    
+    public string roomName { get; }
 
     public PersistentPupData(string id, string room, bool tame)
     {
-        ID = id;
+        PupID = id;
         Room = room;
         IsTame = tame;
+        displayWasTamedInDebug = tame;
     }
 }
